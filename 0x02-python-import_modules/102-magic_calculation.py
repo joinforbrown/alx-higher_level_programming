d2043cd91cce2277674356a574d902213f0ad8dd
joinforbrown <callme508charles@gmail.com>
2024-01-04 07:49:21 -0800

    update task 8&9

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
+
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+        return (c)
 
+    else:
+        return(sub(a, b))
