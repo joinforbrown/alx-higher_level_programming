b0a5168ef4c478b0e6964fd388553629c2d742aa
joinforbrown <callme508charles@gmail.com>
2024-01-09 08:38:09 -0800

    update 1&2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,17 +1,8 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
 def element_at(my_list, idx):
-    # Check if idx is negative
-    if idx < 0:
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
-
-    # Check if idx is out of range
-    if idx >= len(my_list):
-        return None
-
-    # Return the element at the specified index
-    return my_list[idx]
-
-# Example usage
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
-
+    return (my_list[idx])
