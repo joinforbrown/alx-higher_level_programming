6a75f5dd24d5048ca71067d464de37224946749b
joinforbrown <callme508charles@gmail.com>
2024-01-23 08:57:38 -0800

    Task 0-6

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new instance of the class.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
+
