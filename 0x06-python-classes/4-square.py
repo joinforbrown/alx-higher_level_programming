2fb7c9c6f54d397fe89570339333811e6dea43d0
joinforbrown <callme508charles@gmail.com>
2024-01-24 03:22:51 -0800

    Updated Task 0-6

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
 class Square:
-
     def __init__(self, size=0):
         self.size = size
 
-    @property
     def size(self):
         return (self.__size)
 
-    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -19,4 +16,6 @@ class Square:
 
     def area(self):
         return (self.__size * self.__size)
+
+
 
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,11 +5,9 @@ class Square:
         self.size = size
         self.position = position
 
-    @property
     def size(self):
-        return (self.__size)
+        return self.__size
 
-    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -17,30 +15,41 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property
     def position(self):
-        return (self.__position)
+        return self.__position
 
-    @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
+        if (not isinstance(value, tuple) or len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        return (self.__size * self.__size)
+        return self.__size ** 2
 
     def my_print(self):
         if self.__size == 0:
-            print("")
-            return
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
 
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    my_square_1.my_print()
+
+    print("--")
+
+    my_square_2 = Square(3, (1, 1))
+    my_square_2.my_print()
+
+    print("--")
+
+    my_square_3 = Square(3, (3, 0))
+    my_square_3.my_print()
+
+    print("--")
 
