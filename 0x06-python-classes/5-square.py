bc92b81e5b42f0718760d56bae411e3e5532e98d
joinforbrown <callme508charles@gmail.com>
2024-01-24 02:18:08 -0800

    Task 0-6 updated

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,43 +1,29 @@
 #!/usr/bin/python3
+
 class Square:
-    def __init__(self, size=0):
+
+    def __init__(self, size):
         self.size = size
 
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        return self.__size ** 2
+        return (self.__size * self.__size)
 
     def my_print(self):
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
-
-if __name__ == "__main__":
-    my_square = Square(3)
-    my_square.my_print()
-
-    print("--")
-
-    my_square.size = 10
-    my_square.my_print()
-
-    print("--")
-
-    my_square.size = 0
-    my_square.my_print()
-
-    print("--")
+            print("")
 
