3b0cabb27c901346295d84894d49cb5686864932
joinforbrown <callme508charles@gmail.com>
2024-02-05 09:15:26 -0800

    updated task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
+"""Module that defines a Square"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
+
+
 class Square(Rectangle):
+    """Square class, inherits from Rectangle"""
+
     def __init__(self, size):
+        """Initialize a Square instance"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        """Compute the area of the Square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return a string representation of the Square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
