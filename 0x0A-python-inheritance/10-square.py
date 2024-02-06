6ccac387823625840b09a87c7213aa9aff983a46
joinforbrown <callme508charles@gmail.com>
2024-02-06 05:40:18 -0800

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,61 +1,17 @@
 #!/usr/bin/python3
-
-""" Module - 9-rectangle """
-
-class BaseGeometry:
-    """
-    Class BaseGeometry
-    """
-    def area(self):
-        """
-        Raise an exception with an error message
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate value
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """
-    Rectangle class, inheritance from BaseGeometry
-    """
-    def __init__(self, width, height):
-        """
-        Constructor
-        """
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Area of rectangle
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Return a string
-        """
-        return f"[Rectangle] {self.__width}/{self.__height}"
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Square class, inheritance from Rectangle
-    """
-    def __init__(self, size):
-        """
-        Constructor
-        """
-        super().__init__(size, size)
+    """Represent a square."""
 
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
