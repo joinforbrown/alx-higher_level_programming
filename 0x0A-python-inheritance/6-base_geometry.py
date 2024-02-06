7b2fb7b386bc245219bb7d2ebcd35ac7f4907393
joinforbrown <callme508charles@gmail.com>
2024-02-06 05:24:21 -0800

    update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    def area(self):
-        raise Exception("area() is not implemented")
+"""Defines a base geometry class BaseGeometry."""
 
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
